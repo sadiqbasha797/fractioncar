0621a9975b9e3794d3c71ff97c0e35a545cd83de
sadiqbasha797 <msadiqbasha777@gmail.com>
2025-09-16 03:27:15 +0530

    backend

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ app.use('/api/amc-reminders', amcReminderRoutes);
 const dashboardRoutes = require('./routes/dashboardRoutes');
 app.use('/api/dashboard', dashboardRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 2000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   // Start cron jobs for scheduled tasks
